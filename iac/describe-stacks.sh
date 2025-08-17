7e1c599e65c3bcdb20dc247dbd6db384a795317d
Jacob Urmanec <jurmanec@gmail.com>
2025-08-17 13:42:28 -0400

    add amplify.yml

--- iac/describe-stacks.sh
+++ iac/describe-stacks.sh
@@ -8,6 +8,6 @@ backend=order-management-dashboard-backend
 
 export API_URL=$(aws cloudformation describe-stacks --stack-name $backend-$env --query 'Stacks[0].Outputs[?OutputKey==`ServiceEndpoint`].OutputValue' --output text)
 export WEBSOCKET_URL=$(aws cloudformation describe-stacks --stack-name $backend-$env --query 'Stacks[0].Outputs[?OutputKey==`ServiceEndpointWebsocket`].OutputValue' --output text)
-echo "NEXT_PUBLIC_API_URL=$API_URL" >> .env.production
+echo "NEXT_PUBLIC_API_URL=$API_URL/orders" >> .env.production
 echo "NEXT_PUBLIC_WEBSOCKET_URL=$API_URL" >> .env.production
 
